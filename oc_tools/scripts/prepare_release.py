f9f097603eedaf638fc3249f97dd416541c69a37
rr- <rr-@sakuya.pl>
2021-08-22 19:39:59 +0200

    prepare-release: create

---
+++ oc_tools/scripts/prepare_release.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import argparse
+import sys
+from pathlib import Path
+from subprocess import run
+
+TARGET_HOST = "oc"
+TARGET_DIR = "srv/torrent/data/"
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("source", type=Path)
+    return parser.parse_args()
+
+
+def main() -> None:
+    args = parse_args()
+
+    run(
+        [
+            "rsync",
+            "-ahsv",
+            "--progress",
+            args.source,
+            f"{TARGET_HOST}:{TARGET_DIR}",
+        ],
+        stdout=sys.stderr.fileno(),
+        check=True,
+    )
+
+    run(
+        [
+            "ssh",
+            TARGET_HOST,
+            "cd srv/website; "
+            f"docker-compose run --rm app manage prepare_release '{args.source.name}'",
+        ],
+        stdout=sys.stderr.fileno(),
+        check=True,
+    )
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except RuntimeError as ex:
+        print(ex, file=sys.stderr)
